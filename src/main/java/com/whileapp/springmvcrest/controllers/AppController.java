96397dd1b3cf1c3ebdc93457f8a026fc61addad2
Eduardo Jimenez <worldjiz@gmail.com>
2015-08-19 21:33:52 -0500

    Prueba de REST, index.html, main.js y main.css (P.e http://localhost:8080/css/main.css)

---
+++ src/main/java/com/whileapp/springmvcrest/controllers/AppController.java
@@ -0,0 +1,18 @@
+package com.whileapp.springmvcrest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AppController {
+    @RequestMapping("/index")
+    public String index(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+}
+
+
